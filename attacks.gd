792f080e7c84ded8373032ecd19bafd684b6a92d
SnapTurtleTom <tompham1705@gmail.com>
2025-06-02 12:27:00 +1000

    toby's attempt at fixing dash

--- attacks.gd
+++ attacks.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var dash_speed = 10.0
+var dash_speed = 30.0
 
 func _physics_process(delta: float) -> void:
 		var mouse_pos = get_global_mouse_position()
@@ -10,7 +10,8 @@ func _physics_process(delta: float) -> void:
 		
 		if Input.is_action_just_pressed("dash"):
 			get_parent().velocity = Vector2.ZERO
-			get_parent().velocity += mouse_pos * dash_speed
+			get_parent().velocity += mouse_vector * dash_speed
+			get_parent().move_and_slide()
 
 func _on_hit(body : Node2D):
 	if body.is_in_group("enemy"):
