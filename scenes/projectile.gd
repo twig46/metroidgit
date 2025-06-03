3715f750d3b1407ecdfeea506f77999d571a3472
SnapTurtleTom <tompham1705@gmail.com>
2025-06-03 22:02:55 +1000

    attempted fix at projectile

--- scenes/projectile.gd
+++ scenes/projectile.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
-var pos = get_node("/root/Node2D/Player/Frog").position
+var bullet_speed = 0
 
 func _ready():
 	$Projectile.visible = false
+	$Projectile.disabled = true
 
-func _physics_process(delta: float) -> void:
-	global_position = Vector2(pos.x, pos.y)
+func _physics_process(delta):
 	var mouse_pos = get_global_mouse_position()
 	var mouse_vector = mouse_pos - get_parent().position
+	position += mouse_vector.normalized() * bullet_speed * delta
 	if Input.is_action_just_pressed("shoot"):
 		$Projectile.visible = true
+		bullet_speed = 100
+		await get_tree().create_timer(3).timeout
+		bullet_speed = 0
+	move_and_slide()
